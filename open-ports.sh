38da954299f1ef25ad8ea81240b91e5a876ba9df
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-01-05 20:06:37 +0530

    Create open-ports.sh

---
+++ open-ports.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+sudo ufw allow 2377
+sudo ufw allow 7946
+sudo ufw allow 4789
+sudo ufw allow 50
+sudo ufw allow 2375
+sudo ufw allow 3375
+sudo ufw allow 8500
+sudo ufw allow 8300
+sudo ufw allow 8301
+sudo ufw allow 8302
